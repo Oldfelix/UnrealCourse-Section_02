db5046811ddf24a9b094cee91b50382f93a62727
Jim Hansen <kermijim@gmail.com>
2016-12-09 22:29:11 -0700

    Section 2 Leture 20 Added PlayGame Seperated GetGuess and PrintGuess funtions

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
 
-
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+string GetGuess();
+void PrintGuess();
+
+string Guess = "";
 
 // Entry point to application
 int main()
 {
-	
-	PrintIntro();
 
-	//loop for the number of turns asking for guesses
-	constexpr int NUMBER_OF_TURNS = 5;
-	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
-	{
-		GetGuessAndPrintBack();
-		cout << endl;
-	}
-	return 0;
+	PrintIntro();
+	PlayGame();
+	
+
+	return 0;//exit the application
 }
 
 //introduce the game
 void PrintIntro()
 {
-	
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows,a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
@@ -35,14 +33,29 @@ void PrintIntro()
 	return;
 }
 
-// get a guess from the player
-string GetGuessAndPrintBack()
+//loop for the number of turns asking for guesses
+void PlayGame()
 {
-	string Guess = "";
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		GetGuess();
+		cout << endl;
+		PrintGuess();
+	}
+}
+
+// get a guess from the player
+string GetGuess()
+{
 	cout << "Please enter your guess: ";
 	getline(cin, Guess);
-
-	// repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
 	return Guess;
+}
+
+// repeat the guess back to them
+void PrintGuess()
+{
+	cout << "Your guess was: " << Guess << endl;
+	return;
 }
